5670bfe0f8ff90b9af72178797260fe75533a776
Bjornlund <irenebjornlund@gmail.com>
2025-01-14 10:34:46 +0100

    Switchcase for sprites

--- Robot bits/Head.cs
+++ Robot bits/Head.cs
@@ -12,13 +12,32 @@ namespace GameJam_Jan_2025
 {
     public class Head : Part
     {
+        
 
 
-        public Head()
+        public Head( Vector2 placement, int robotType)
         {
-            sprite = Gameworld.sprites["head1"];
-            position = new Vector2(1000, 1000);
+            position = placement;
             layer = 1;
+            scale = 0.5f;
+
+            switch (robotType)
+            {
+                case 1:
+                    this.sprite = Gameworld.sprites["head1"];
+                    break;
+                case 2:
+                    this.sprite = Gameworld.sprites["head2"];
+                    break;
+                case 3:
+                    this.sprite = Gameworld.sprites["head3"];
+                    break;
+                default:
+                    break;
+
+            }
         }
+
+        
     }
 }
